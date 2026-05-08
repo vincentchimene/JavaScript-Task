25160bf5699c53ce66d1055a6b621e6b33f4fb65
Vincent Opurum <vincentchimene@gmail.com>
2026-05-08 03:06:06 +0100

    adds task Nine of Meatpie

---
+++ meatpie/TaskNine.js
@@ -0,0 +1,5 @@
+// A JavaScript program that asks user's favourite colour and prints it.
+const prompt = require("prompt-sync")();
+
+let color = prompt("What is your favourite color? ");
+console.log(`Your favourite color is ${color}`);
