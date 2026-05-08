e9a290e6f2b0ec94476591844fade50a1f58b1cf
Vincent Opurum <vincentchimene@gmail.com>
2026-05-08 01:39:49 +0100

    adds task six of meatPie task

---
+++ meatpie/TaskSix.js
@@ -0,0 +1,13 @@
+//program that declares a variable counter and uses the pre-increment 
+//operator three times, printing the value of the counter after each increment.
+
+let counter = 0;
+console.log(`Initial counter = ${counter}`);
+++counter;
+console.log(`New counter = ${counter}`);
+++counter;
+console.log(`New counter = ${counter}`);
+++counter;
+console.log(`New counter = ${counter}`);
+
+
